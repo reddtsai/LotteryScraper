d04a986c66eaa4c773b81d7c8ba07dd0d4be8cc8
ReddTsai <reddtsai@gmail.com>
2020-03-31 16:42:54 +0800

    fix deploy script

--- gce.sh
+++ gce.sh
@@ -19,10 +19,17 @@ export HOME=/root
 # git clone -b f_CreateDeployment --single-branch https://github.com/reddtsai/pythonLotteryScraper.git /opt/app
 git clone https://github.com/reddtsai/pythonLotteryScraper.git /opt/app
 
+# Python environment setup
+virtualenv -p python3 /opt/app/gce/env
+source /opt/app/gce/env/bin/activate
+/opt/app/gce/env/bin/pip install -r /opt/app/requirements.txt
+
 # Setup cloud_sql_proxy on system service
 cp /opt/app/cloud-sql-proxy.service /etc/systemd/system/cloud-sql-proxy.service
 systemctl enable cloud-sql-proxy.service
 systemctl start cloud-sql-proxy.service
+
+sleep 15
 
 # Scheduling scraper on system service
 cp /opt/app/lotto-scraper.service /etc/systemd/system/lotto-scraper.service
@@ -31,11 +38,6 @@ systemctl start lotto-scraper.service
 cp /opt/app/lotto-scraper.timer /etc/systemd/system/lotto-scraper.timer
 systemctl enable lotto-scraper.timer
 systemctl start lotto-scraper.timer
-
-# Python environment setup
-virtualenv -p python3 /opt/app/gce/env
-source /opt/app/gce/env/bin/activate
-/opt/app/gce/env/bin/pip install -r /opt/app/requirements.txt
 
 # 設定擁有者:群組。目錄下所有子目錄和檔案(-R)
 # chown -R pythonapp:pythonapp /opt/app
